1b2318f19e1bdb575656d383e313e9eca0e324d6
dduartevla <deboraduarte@ice.ufjf.br>
2023-05-02 21:23:20 -0300

    Comit Inicial

---
+++ src/test/java/dcc/ufjf/dcc193/debora/tomato/TomatoApplicationTests.java
@@ -0,0 +1,13 @@
+package dcc.ufjf.dcc193.debora.tomato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
