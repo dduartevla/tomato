f9e3a8184fbf9b5293344214fd4af530868aae7c
dduartevla <deboraduarte@ice.ufjf.br>
2023-05-30 11:54:04 -0300

    Adiciona edição e exclusão de usuários

--- src/main/java/dcc/ufjf/dcc193/debora/tomato/UsuarioController.java
+++ src/main/java/dcc/ufjf/dcc193/debora/tomato/UsuarioController.java
@@ -1,11 +1,13 @@
 package dcc.ufjf.dcc193.debora.tomato;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -58,5 +60,37 @@ public class UsuarioController {
         List<Usuario> usuarios = repUsuario.findAll();
         mv.addObject("usuarios", usuarios);
         return mv;
+    }
+
+    @GetMapping("/editarUsuario/{id}")
+    ModelAndView editarForm(@PathVariable Long id){
+        ModelAndView mv = new ModelAndView();
+        Optional<Usuario> opcional = repUsuario.findById(id);
+        if (opcional.isPresent()) {
+            mv.setViewName("usuarios/usuarios-form-edit.html");
+            mv.addObject("usuario", opcional.get());
+            return mv;
+        }
+        mv.setViewName("redirect:/atividades/listarUsuarios.html");
+        return mv;
+    }
+
+    @PostMapping("/editarUsuario/{id}")
+    ModelAndView editarFormPost(@PathVariable Long id, @Valid Usuario usuario, BindingResult binding){
+        ModelAndView mv = new ModelAndView();
+        if (binding.hasErrors()) {
+            mv.setViewName("usuarios/usuarios-form-edit.html");
+            mv.addObject("usuario", usuario);
+            return mv;
+        }
+        repUsuario.save(usuario);
+        mv.setViewName("redirect:/atividades/listarUsuarios.html");
+        return mv;
+    }
+
+    @GetMapping("/excluirUsuario/{id}")
+    String excluir(@PathVariable Long id){
+        repUsuario.deleteById(id);    
+        return "redirect:/atividades/listarUsuarios.html";
     }
 }
