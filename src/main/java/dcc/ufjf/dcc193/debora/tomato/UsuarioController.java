f2276344f3ca4f6f392fe4e39b94e6a83f439226
dduartevla <deboraduarte@ice.ufjf.br>
2023-05-30 02:01:56 -0300

    Faz o menu inicial ir para os usuários

--- src/main/java/dcc/ufjf/dcc193/debora/tomato/UsuarioController.java
+++ src/main/java/dcc/ufjf/dcc193/debora/tomato/UsuarioController.java
@@ -1,5 +1,15 @@
 package dcc.ufjf.dcc193.debora.tomato;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/atividades")
 public class UsuarioController {
     
+    @GetMapping({"","/","/usuariosindex.html"})
+    public String index(){
+        return "usuarios/usuarios-index.html";
+    }
 }
